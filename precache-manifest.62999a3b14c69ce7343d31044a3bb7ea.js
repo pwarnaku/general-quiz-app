ac852812054cd4c534f67f3d284e8e50a51101ff
Piumi Fernando <pwarnaku@deakin.edu.au>
2020-09-18 21:25:35 +1000

    Updates

---
+++ precache-manifest.62999a3b14c69ce7343d31044a3bb7ea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e8370ec9e72da7a71fe81633d7e9b0",
+    "url": "/general-quiz-app/index.html"
+  },
+  {
+    "revision": "eb068aec1384231a55cf",
+    "url": "/general-quiz-app/static/css/main.c29e1bca.chunk.css"
+  },
+  {
+    "revision": "b3cfc6ebee80829e84d0",
+    "url": "/general-quiz-app/static/js/2.abf71f73.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/general-quiz-app/static/js/2.abf71f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb068aec1384231a55cf",
+    "url": "/general-quiz-app/static/js/main.9e15288b.chunk.js"
+  },
+  {
+    "revision": "6045ee5eb0e700c39cbc",
+    "url": "/general-quiz-app/static/js/runtime-main.25a77800.js"
+  },
+  {
+    "revision": "a1cfcb1ef36ac3c26d2cf757c1ebe59a",
+    "url": "/general-quiz-app/static/media/Clicking.a1cfcb1e.mp3"
+  },
+  {
+    "revision": "02882ce29c39d46d8cb8c0eac28ce519",
+    "url": "/general-quiz-app/static/media/Correct.02882ce2.mp3"
+  },
+  {
+    "revision": "6721d28007dcfa6fe23e33320b22b496",
+    "url": "/general-quiz-app/static/media/Error.6721d280.mp3"
+  },
+  {
+    "revision": "f6c13b4bdb896be8b23bde108c3d394d",
+    "url": "/general-quiz-app/static/media/quiz.f6c13b4b.png"
+  }
+]);
